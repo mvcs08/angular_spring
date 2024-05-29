e167dc5aa9d8ab5b1b5e875d510294b5b88bfff7
Israel <israelped10@gmail.com>
2024-05-29 09:28:37 -0300

    feat: implementa Fornecedor

---
+++ login-auth-api/src/main/java/com/example/loginauthapi/DTO/FornecedorDTO.java
@@ -0,0 +1,41 @@
+package com.example.loginauthapi.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class FornecedorDTO {
+    long id;
+
+    String nomeFantasia;
+
+    String razaoSocial;
+
+    String cpfOuCNPJ;
+
+    String inscricaoEstadual;
+
+    //TODO Endereco endereco;
+
+    String tipo;
+
+    String representante;
+
+    String numeroRepresentante;
+
+    String email;
+
+    String emailFinanceiro;
+
+    String dadosBancarios;
+
+    String outros;
+
+}
---
+++ login-auth-api/src/main/java/com/example/loginauthapi/Repositories/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.example.loginauthapi.Repositories;
+
+import com.example.loginauthapi.Domain.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository <Fornecedor, Long> {
+}
