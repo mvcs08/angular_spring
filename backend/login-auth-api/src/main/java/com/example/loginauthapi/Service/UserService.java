49770d137fe17ae7c35dada556ca6eb618c0bf9a
QuevedoMatheus <matheusquevedobio@gmail.com>
2024-06-07 23:45:36 -0300

    Correção do enconder em userService

--- backend/login-auth-api/src/main/java/com/example/loginauthapi/Service/UserService.java
+++ backend/login-auth-api/src/main/java/com/example/loginauthapi/Service/UserService.java
@@ -5,15 +5,20 @@ import com.example.loginauthapi.Domain.Colaborador;
 import com.example.loginauthapi.Domain.User;
 import com.example.loginauthapi.Infra.Security.TokenService;
 import com.example.loginauthapi.Repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
 
@@ -24,9 +29,8 @@ public class UserService {
         User user1 = userRepository.findById(id).orElseThrow(()-> new RuntimeException());
         user1.setName(user.getName());
         user1.setEmail(user.getEmail());
-        String token = this.tokenService.generateToken(user);
-        user1.setPassword(token);
-        return userRepository.save(user);
+        user1.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user1);
     }
 
     public void delete(String id) {
