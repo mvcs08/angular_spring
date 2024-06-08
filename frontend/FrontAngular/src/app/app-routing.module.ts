08163c242492dd9a12691fa1e75f7c8aa4af42cf
Eduardo Vasconcelos <107057997+EduardoVasconceloss@users.noreply.github.com>
2024-06-08 18:13:15 -0300

    Update app-routing.module.ts

--- frontend/FrontAngular/src/app/app-routing.module.ts
+++ frontend/FrontAngular/src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './auth.guard';
+import { ListagemFormComponent } from './components/listagem-form/listagem-form.component';
+import { DetalhesComponent } from './pages/detalhes/detalhes.component';
+import { ColaboradorComponent } from './pages/colaborador/colaborador.component';
+import { ServicoComponent } from './pages/servico/servico.component';
+import { EditarUsuarioComponent } from './pages/editar-usuario/editar-usuario.component';
+import { DetalhesColaboradorComponent } from './pages/detalhes-colaborador/detalhes-colaborador.component';
+import { DetalhesServicoComponent } from './pages/detalhes-servico/detalhes-servico.component';
 import { CadastrarEditarColaboradorComponent } from './pages/cadastrar-editar-colaborador/cadastrar-editar-colaborador.component';
 import { CadastrarEditarFornecedorComponent } from './pages/cadastrar-editar-fornecedor/cadastrar-editar-fornecedor.component';
 import { CadastrarEditarServicoComponent } from './pages/cadastrar-editar-servico/cadastrar-editar-servico.component';
@@ -35,6 +42,34 @@ const routes: Routes = [
   {
     path: "cadastrarEditarServiço",
     component: CadastrarEditarServicoComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: "fornecedor",
+    component: ListagemFormComponent
+  },
+  {
+    path: "fornecedor/visualizar/:id",
+    component: DetalhesComponent
+  },
+  {
+    path: "colaborador",
+    component: ColaboradorComponent
+  },
+  {
+    path: "colaborador/visualizar/:id",
+    component: DetalhesColaboradorComponent
+  },
+  {
+    path: "servico",
+    component: ServicoComponent
+  },
+  {
+  path: "servico/visualizar/:id",
+  component: DetalhesServicoComponent
+  },
+  {
+    path: "editarUsuario",
+    component: EditarUsuarioComponent, canActivate: [AuthGuard]
   }
 ];
 
